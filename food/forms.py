b4d4d01270a125a4200348152f4edfbe4665930e
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2022-12-21 04:00:02 +0300

    created forms

--- food/forms.py
+++ food/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User,Profile
 from django.contrib.auth.forms import UserCreationForm
 
 passwordInputWidget = {'password':forms.PasswordInput()}
@@ -20,9 +20,29 @@ class SignUpForm(UserCreationForm):
             user.save()
         return user
 
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-class LoginUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username']
-        widgets = [passwordInputWidget]
+        fields = ['username', 'email']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
+
+
+# class LoginUserForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ['username,password']
+#         widgets = [passwordInputWidget]
