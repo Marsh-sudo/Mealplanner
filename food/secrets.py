228e1a28b4354181b635f89c902c2b3fcf97b935
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2022-12-21 03:58:45 +0300

    random cleanup

--- food/secrets.py
+++ food/secrets.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-apiKey = "Api key"
+apiKey = "332a8c3baef140dab8cb40493a0990bc"
 
 def get_random():
     response = requests.get(f"https://api.spoonacular.com/recipes/random?number=5&tags=main+course&apiKey={apiKey}")
