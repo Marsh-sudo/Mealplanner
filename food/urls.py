777e0e6d672cb3490b123cb52c99ec78ce53d272
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2022-12-21 03:57:07 +0300

    create urls for views

--- food/urls.py
+++ food/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+from food.views import ChangePasswordView
 
 urlpatterns=[
     path("",views.home,name="home"),
     path('signup/', views.signup, name='signup'),
-    path('login',views.login,name="login"),
+    path('login',views.login_request,name="login"),
+    path('profile/', views.profile, name='users-profile'),
+    path("logout", views.logout_request, name= "logout"),
     path("search/",views.search,name="search"),
     path("wines",views.wines,name="wines"),
-    path("wineSearch/",views.wines_pairing,name="wineSearch")
+    path("wineSearch/",views.wines_pairing,name="wineSearch"),
+    path('password-change/', ChangePasswordView.as_view(), name='password_change'),
 
-]
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
